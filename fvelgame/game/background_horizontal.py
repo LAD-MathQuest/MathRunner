a8ee01f3a231ac14befaf4cf665c49c2774e4245
Luis A. D Afonseca <luis.dafonseca@gmail.com>
2023-01-08 15:30:37 -0300

    Implementing the background and track scrolling

--- fvelgame/game/background_horizontal.py
+++ fvelgame/game/background_horizontal.py
@@ -5,55 +5,7 @@ import math
 
 import game.game_params as gp
 
-#------------------------------------------------------------------------------#
-def surface_tiling(surf, tile):
-
-    t_width  = tile.get_width ()
-    t_height = tile.get_height()
-
-    s_width  = surf.get_width ()
-    s_height = surf.get_height()
-
-    rect = pygame.Rect((0,0), (t_width,t_height))
-
-
-    step_x = t_width
-    step_y = t_height
-
-    nh = math.ceil(s_height / t_height)
-    nw = math.ceil(s_width  / t_width )
-
-    rr = rect.copy()
-
-    for ii in range(nh):
-        for jj in range(nw):
-            surf.blit(tile, rr)
-            rr.move_ip(step_x, 0)
-
-        rr.left = rect.left
-        rr.move_ip(0, step_y)
-
-#------------------------------------------------------------------------------#
-def fit_image_to_screen(image):
-
-    im_width  = image.get_width ()
-    im_height = image.get_height()
-
-    sc_width  = gp.SCREEN_SIZE[0]
-    sc_height = gp.SCREEN_SIZE[1]
-
-    new_image = pygame.Surface(gp.SCREEN_SIZE)
-
-    if im_width < sc_width or im_height < sc_height:
-        surface_tiling(new_image, image)
-
-    else: # Image has same size or is larger than screen
-
-        rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
-
-        new_image.blit(image, (0,0), rect)
-
-    return new_image
+from game.tools import surface_tile_to_size
 
 #------------------------------------------------------------------------------#
 class BackgroundHorizontal:
@@ -65,97 +17,80 @@ class BackgroundHorizontal:
         self.boundary = world.boundary
 
         self.bg_scrolls = world.background_scrolls
+        self.tr_scrolls = world.track_scrolls
+
+        # Background initialization
+
+        image = self.world.background_image
+        width = image.get_width()
+
+        new_size = list(gp.SCREEN_SIZE)
 
         if self.bg_scrolls:
-            self.set_background_scrolling()
-        else:
-            self.bg_image     = fit_image_to_screen(world.background_image)
-            self.bg_show_rect = None
+            new_size[0] += width
+
+        self.bg_image     = surface_tile_to_size(image, new_size)
+        self.bg_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
+        self.bg_max_width = new_size[0]
 
         # Initializing track
 
-        self.tr_image   = world.track_image
-        self.tr_scrolls = world.track_scrolls
+        self.draw_track = bool(world.track_image)
+
+        if self.draw_track:
+
+            image = self.world.track_image
+            width = image.get_width()
+
+            new_size = list(gp.SCREEN_SIZE)
+
+            if self.tr_scrolls:
+                new_size[0] += width
+
+            self.tr_image     = surface_tile_to_size(image, new_size)
+            self.tr_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
+            self.tr_max_width = new_size[0]
+
+        # Initializing boundary
 
         tr_min, lenght = self.boundary.eval_length(0)
 
         top    = int(gp.SCREEN_SIZE[1] * tr_min)
         height = int(gp.SCREEN_SIZE[1] * lenght)
-        self.tr_rect = pygame.Rect(0, top, gp.SCREEN_SIZE[0], height)
 
-        if self.tr_image:
-            self.bg_image.blit(self.tr_image, self.tr_rect, self.tr_rect)
+        self.bd_rect = pygame.Rect(0, top, gp.SCREEN_SIZE[0], height)
 
-    #--------------------------------------------------------------------------#
-    def set_background_scrolling(self):
-
-        w_image  = self.world.background_image
-        w_width  = w_image.get_width ()
-        w_height = w_image.get_height()
-
-        s_width  = gp.SCREEN_SIZE[0]
-        s_height = gp.SCREEN_SIZE[1]
-
-        if w_width < s_width or w_height < s_height:
-
-            n_width = math.ceil(s_width / w_width) * w_width
-            n_image = pygame.Surface((n_width, s_height))
-
-            surface_tiling(n_image, w_image)
-
-            w_image  = n_image
-            w_width  = n_width
-            w_height = s_height
-
-        self.bg_max_width = w_width + s_width
-        self.bg_image     = pygame.Surface((self.bg_max_width, s_height))
-
-        self.bg_image.blit(w_image, (0,0))
-        self.bg_image.blit(w_image, (w_width,0), (0,0,s_width,s_height) )
-
-        self.bg_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
-        self.bg_scrolls   = self.world.background_scrolls
-
-        if not self.bg_scrolls:
-            self.bg_image     = self.world.background_image
-            self.bg_show_rect = None
-            return
-
-        w_bg_image  = self.world.background_image
-        w_bg_width  = w_bg_image.get_width ()
-        w_bg_height = w_bg_image.get_height()
-
-        sc_width  = gp.SCREEN_SIZE[0]
-        sc_height = gp.SCREEN_SIZE[1]
-
-        self.bg_max_width = w_bg_width + sc_width
-        self.bg_image     = pygame.Surface((self.bg_max_width, sc_height))
-
-        self.bg_image.blit(w_bg_image, (0,0))
-        self.bg_image.blit(w_bg_image, (sc_width+1,0), (0,0,sc_width,sc_height) )
-
-        self.bg_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
+        if self.draw_track:
+            self.tr_show_rect.top    = top
+            self.tr_show_rect.height = height
 
     #--------------------------------------------------------------------------#
     def update(self, displacement):
 
-        if not self.bg_scrolls: return
+        if self.bg_scrolls:
+            self.bg_show_rect.right += displacement
+            if self.bg_show_rect.right > self.bg_max_width:
+                self.bg_show_rect.right += gp.SCREEN_SIZE[0] - self.bg_max_width
 
-        self.bg_show_rect.right += displacement
-
-        if self.bg_show_rect.right > self.bg_max_width:
-            self.bg_show_rect.right += gp.SCREEN_SIZE[0] - self.bg_max_width
+        if self.tr_scrolls:
+            self.tr_show_rect.right += displacement
+            if self.tr_show_rect.right > self.tr_max_width:
+                self.tr_show_rect.right += gp.SCREEN_SIZE[0] - self.tr_max_width
 
     #--------------------------------------------------------------------------#
     def draw(self, surf):
+
         surf.blit(self.bg_image, (0,0), self.bg_show_rect)
+
+        if self.draw_track:
+            surf.blit(self.tr_image, self.bd_rect, self.tr_show_rect)
 
     #--------------------------------------------------------------------------#
     def get_player_boundaries(self):
-        return [self.tr_rect.top, self.tr_rect.bottom]
+        return [self.bd_rect.top, self.bd_rect.bottom]
 
     #--------------------------------------------------------------------------#
     def get_spawn_boundaries(self):
-        return [self.tr_rect.top, self.tr_rect.bottom]
+        return [self.bd_rect.top, self.bd_rect.bottom]
 
 #------------------------------------------------------------------------------#
--- fvelgame/game/background_vertical.py
+++ fvelgame/game/background_vertical.py
@@ -5,55 +5,7 @@ import math
 
 import game.game_params as gp
 
-#------------------------------------------------------------------------------#
-def surface_tiling(surf, tile):
-
-    t_width  = tile.get_width ()
-    t_height = tile.get_height()
-
-    s_width  = surf.get_width ()
-    s_height = surf.get_height()
-
-    rect = pygame.Rect((0,0), (t_width,t_height))
-    rect.bottom = s_height
-
-    step_x =  t_width
-    step_y = -t_height
-
-    nh = math.ceil(s_height / t_height)
-    nw = math.ceil(s_width  / t_width )
-
-    rr = rect.copy()
-
-    for ii in range(nh):
-        for jj in range(nw):
-            surf.blit(tile, rr)
-            rr.move_ip(step_x, 0)
-
-        rr.left = rect.left
-        rr.move_ip(0, step_y)
-
-#------------------------------------------------------------------------------#
-def fit_image_to_screen(image):
-
-    im_width  = image.get_width ()
-    im_height = image.get_height()
-
-    sc_width  = gp.SCREEN_SIZE[0]
-    sc_height = gp.SCREEN_SIZE[1]
-
-    new_image = pygame.Surface(gp.SCREEN_SIZE)
-
-    if im_width < sc_width or im_height < sc_height:
-        surface_tiling(new_image, image)
-
-    else: # Image has same size or is larger than screen
-
-        rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
-        rect.bottom = im_height
-        new_image.blit(image, (0,0), rect)
-
-    return new_image
+from game.tools import surface_tile_to_size
 
 #------------------------------------------------------------------------------#
 class BackgroundVertical:
@@ -65,97 +17,80 @@ class BackgroundVertical:
         self.boundary = world.boundary
 
         self.bg_scrolls = world.background_scrolls
+        self.tr_scrolls = world.track_scrolls
+
+        # Background initialization
+
+        image  = self.world.background_image
+        height = image.get_height()
+
+        new_size = list(gp.SCREEN_SIZE)
 
         if self.bg_scrolls:
-            self.set_background_scrolling()
-        else:
-            self.bg_image     = fit_image_to_screen(world.background_image)
-            self.bg_show_rect = None
+            new_size[1] += height
+
+        self.bg_image     = surface_tile_to_size(image, new_size, True)
+        self.bg_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
+        self.bg_top_start = height
 
         # Initializing track
 
-        self.tr_image   = world.track_image
-        self.tr_scrolls = world.track_scrolls
+        self.draw_track = bool(world.track_image)
+
+        if self.draw_track:
+
+            image  = self.world.track_image
+            height = image.get_height()
+
+            new_size = list(gp.SCREEN_SIZE)
+
+            if self.tr_scrolls:
+                new_size[1] += height
+
+            self.tr_image     = surface_tile_to_size(image, new_size, True)
+            self.tr_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
+            self.tr_top_start = height
+
+        # Initializing boundary
 
         tr_min, lenght = self.boundary.eval_length(0)
 
         left  = int(gp.SCREEN_SIZE[0] * tr_min)
         width = int(gp.SCREEN_SIZE[0] * lenght)
-        self.tr_rect = pygame.Rect(left, 0, width, gp.SCREEN_SIZE[1])
 
-        if self.tr_image:
-            self.bg_image.blit(self.tr_image, self.tr_rect, self.tr_rect)
+        self.bd_rect = pygame.Rect(left, 0, width, gp.SCREEN_SIZE[1])
 
-    #--------------------------------------------------------------------------#
-    def set_background_scrolling(self):
-
-        w_image  = self.world.background_image
-        w_width  = w_image.get_width ()
-        w_height = w_image.get_height()
-
-        s_width  = gp.SCREEN_SIZE[0]
-        s_height = gp.SCREEN_SIZE[1]
-
-        if w_width < s_width or w_height < s_height:
-
-            n_height = math.ceil(s_height / w_height) * w_height
-            n_image  = pygame.Surface((s_width, n_height))
-
-            surface_tiling(n_image, w_image)
-
-            w_image  = n_image
-            w_width  = s_width
-            w_height = n_height
-
-        self.bg_top_start = w_height
-        self.bg_image     = pygame.Surface((s_width, w_height+s_height))
-
-        self.bg_image.blit(w_image, (0,s_height))
-        self.bg_image.blit(w_image, (0,0), (0,w_height-s_height,s_width,s_height) )
-
-        self.bg_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
-        self.bg_scrolls   = self.world.background_scrolls
-
-        if not self.bg_scrolls:
-            self.bg_image     = self.world.background_image
-            self.bg_show_rect = None
-            return
-
-        w_bg_image  = self.world.background_image
-        w_bg_width  = w_bg_image.get_width ()
-        w_bg_height = w_bg_image.get_height()
-
-        sc_width  = gp.SCREEN_SIZE[0]
-        sc_height = gp.SCREEN_SIZE[1]
-
-        self.bg_top_start = w_bg_height
-        self.bg_image     = pygame.Surface((sc_width, w_bg_height+sc_height))
-
-        self.bg_image.blit(w_bg_image, (0,sc_height))
-        self.bg_image.blit(w_bg_image, (0,0), (0,w_bg_height-sc_height,sc_width,sc_height) )
-
-        self.bg_show_rect = pygame.Rect((0,0), gp.SCREEN_SIZE)
+        if self.draw_track:
+            self.tr_show_rect.left  = left
+            self.tr_show_rect.width = width
 
     #--------------------------------------------------------------------------#
     def update(self, displacement):
 
-        if not self.bg_scrolls: return
+        if self.bg_scrolls:
+            self.bg_show_rect.top -= displacement
+            if self.bg_show_rect.top < 0:
+                self.bg_show_rect.top = self.bg_top_start + self.bg_show_rect.top
 
-        self.bg_show_rect.top -= displacement
-
-        if self.bg_show_rect.top < 0:
-            self.bg_show_rect.top = self.bg_top_start + self.bg_show_rect.top
+        if self.tr_scrolls:
+            self.tr_show_rect.top -= displacement
+            if self.tr_show_rect.top < 0:
+                self.tr_show_rect.top = self.tr_top_start + self.tr_show_rect.top
 
     #--------------------------------------------------------------------------#
     def draw(self, surf):
+
         surf.blit(self.bg_image, (0,0), self.bg_show_rect)
+
+        if self.draw_track:
+            surf.blit(self.tr_image, self.bd_rect, self.tr_show_rect)
 
     #--------------------------------------------------------------------------#
     def get_player_boundaries(self):
-        return [self.tr_rect.left, self.tr_rect.right]
+        return [self.bd_rect.left, self.bd_rect.right]
 
     #--------------------------------------------------------------------------#
     def get_spawn_boundaries(self):
-        return [self.tr_rect.left, self.tr_rect.right]
+        return [self.bd_rect.left, self.bd_rect.right]
 
 #------------------------------------------------------------------------------#
