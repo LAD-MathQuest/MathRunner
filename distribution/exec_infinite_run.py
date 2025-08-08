c3259d5ae16e6a34fc8d1a955bcb7b1340e2cf7a
Samuel Lopes <samuelopes2003@gmail.com>
2025-08-08 14:01:46 -0300

    Correção de sintaxe no arquivo exec_infinite_run

--- distribution/exec_infinite_run.py
+++ distribution/exec_infinite_run.py
@@ -1,13 +1,12 @@
 #------------------------------------------------------------------------------#
 
 import sys
-<<<<<<<< HEAD:src/math_runner/__main__.py
 from . import gui
 
 #------------------------------------------------------------------------------#
 if __name__ == '__main__':
     sys.exit(gui.main(sys.argv))
-========
+
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent / 'src'))
 
@@ -16,6 +15,5 @@ from infinite_run import game
 #------------------------------------------------------------------------------#
 if __name__ == '__main__':
     sys.exit(game.main())
->>>>>>>> master:distribution/exec_infinite_run.py
 
 #------------------------------------------------------------------------------#
